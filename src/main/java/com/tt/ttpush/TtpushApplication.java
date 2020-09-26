87fcfdce61e25355752815d062dc559c1926bd72
jiuyihua <jiuyi_huhua@163.com>
2020-09-26 12:57:52 +0800

    测试push

---
+++ src/main/java/com/tt/ttpush/TtpushApplication.java
@@ -0,0 +1,13 @@
+package com.tt.ttpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtpushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtpushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tt/ttpush/TtpushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.ttpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtpushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
