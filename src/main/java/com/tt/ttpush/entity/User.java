76185992b2581b00fcd55aed1ef6fcfd3759d7fa
jiuyihua <jiuyi_huhua@163.com>
2020-09-26 14:45:06 +0800

    测试push

---
+++ src/main/java/com/tt/ttpush/entity/User.java
@@ -0,0 +1,17 @@
+package com.tt.ttpush.entity;
+
+import javax.annotation.sql.DataSourceDefinition;
+
+/**
+ * describle :
+ * author : huh
+ * 2020/9/26 0026
+ * 下午 2:43
+ */
+public class User {
+
+    private Integer id;
+    private String name;
+
+
+}
