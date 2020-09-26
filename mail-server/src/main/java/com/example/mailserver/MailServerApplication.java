6cccfde490a34e9f701f7697e44c23dc3e17b658
jiuyihua <jiuyi_huhua@163.com>
2020-09-26 14:53:36 +0800

    增加邮件功能

--- mail-server/src/main/java/com/example/mailserver/MailServerApplication.java
+++ mail-server/src/main/java/com/example/mailserver/MailServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MailServerApplication {
 
+    //增加邮件功能
     public static void main(String[] args) {
         SpringApplication.run(MailServerApplication.class, args);
     }
